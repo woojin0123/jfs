6391b4ba64dd8507fc6a1b90919848d839ff7a3f
woojin0123 <iamwjwj@gmail.com>
2025-11-28 16:14:19 +0900

    1128

--- js/counter.js
+++ js/counter.js
@@ -1,47 +1,46 @@
 $(function () {
-  function runCounter($counter) {
-    var target = parseInt($counter.attr("data-target")),
-      count = 0,
-      speed = 50;
-    // 0부터 출발
-    $counter.text(0);
+    function runCounter($counter) {
+        var target = parseInt($counter.attr("data-target")),
+            count = 0,
+            speed = 50;
 
-    var counterInterval = setInterval(function () {
-      count += Math.ceil(target / 100);
-      if (count >= target) {
-        count = target;
-        clearInterval(counterInterval);
-      }
-      $counter.text(count);
-    }, speed);
-  } // function runCounter($counter)
+        $counter.text(0);
 
-  function checkScroll() {
-    const scrollTop = $(window).scrollTop(),
-      windowHeight = $(window).height(),
-      elementTop = $("#counters").offset().top;
+        var counterInterval = setInterval(function () {
+            count++;
+            if (count >= target) {
+                clearInterval(counterInterval);
+            }
+            // 바 증가
+            $counter.parent().css("width", count + "%");
+            // 숫자(퍼센트) 증가
+            $counter.text(count);
+        }, speed);
+    } // function runCounter($counter) end
 
-    $(".counter").each(function () {
-      const $this = $(this);
-      // elementTop = $this.offset().top;
+    function checkScroll() {
+        const scrollTop = $(window).scrollTop(),
+            windowHeight = $(window).height(),
+            elementTop = $(".counters").offset().top;
 
-      if (scrollTop + windowHeight > elementTop + 100) {
-        // if (scrollTop + windowHeight > elementTop + 50 && scrollTop < elementTop + $this.outerHeight()) {
-        // 위 if가 참이면 실행
-        if (!$this.hasClass("active")) {
-          $this.addClass("active");
-          runCounter($this);
-        }
-      } else {
-        $this.removeClass("active");
-        $this.text(0);
-      }
-    });
-  } // function checkScroll()
+        $(".counter").each(function () {
+            const $this = $(this);
+            //console.log($this);
 
-  // $.throttle(시간, 함수):
-  // 시간: ms(밀리세컨드), ex. 100: 0.1초
-  // $(window).on("scroll", $.throttle(100, checkScroll));
-  $(window).on("scroll", $.throttle(100, checkScroll));
-  checkScroll();
-});
+            if (scrollTop + windowHeight > elementTop + 100) {
+                if (!$this.hasClass("active")) {
+                    $this.addClass("active");
+                    runCounter($this);
+                }
+            } else {
+                $this.removeClass("active");
+                $this.text(0);
+            }
+        });
+        //console.log($(window).scrollTop());
+    } // function checkScroll() end
+
+    $(window).on("scroll", $.throttle(100, checkScroll));
+    checkScroll();
+
+}); // $(document).ready() end
--- js/progress.js
+++ js/progress.js
@@ -1,22 +1,20 @@
 $(function () {
-            let current = 0;            // 시작값
-            let target = 100;           // 목표값
-            let speed = 20;             // 20ms(=0.02초) 마다 업데이트
+    let current = 0; // 시작값
+    let target = 100; // 목표값
+    let speed = 20; // 20ms(=0.02초) 마다 업데이트
 
-            let timer = setInterval(function () {
+    let timer = setInterval(function () {
+        current++;
 
-                current++;
+        // 진행바 채우기
+        $(".progress-bar").css("width", current + "%");
 
-                // 진행바 채우기
-                $(".progress-bar").css("width", current + "%");
+        // 숫자 갱신
+        $(".percent").text(current + "%");
 
-                // 숫자 갱신
-                $(".percent").text(current + "%");
-
-                // 100% 되면 정지
-                if (current >= target) {
-                    clearInterval(timer);
-                }
-
-            }, speed);
-        });
+        // 100% 되면 정지
+        if (current >= target) {
+            clearInterval(timer);
+        }
+    }, speed);
+});
